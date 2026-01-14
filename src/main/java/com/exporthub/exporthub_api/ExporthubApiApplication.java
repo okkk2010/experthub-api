11bb9e29f1888a1c0388a9f329160b73ac243e5a
okkk2010 <okkk2010@naver.com>
2026-01-14 10:12:18 +0900

    first commit

---
+++ src/main/java/com/exporthub/exporthub_api/ExporthubApiApplication.java
@@ -0,0 +1,13 @@
+package com.exporthub.exporthub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExporthubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExporthubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exporthub/exporthub_api/ExporthubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exporthub.exporthub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExporthubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
